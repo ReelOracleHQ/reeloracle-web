0f71cc82dcf74a7645374a12e839743282d73694
ReelOracleHQ <reeloraclehq@gmail.com>
2026-03-12 12:39:30 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'reeloracle-navy': '#0A0F1E',
+        'cyber-blue': '#00D4FF',
+        'verified-green': '#10B981'
+      }
+    }
+  },
+  plugins: []
+}
